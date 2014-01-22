20b2d0b8ee3a7a3050c94d0636d983b45aa7cd96
stephanieguo <stguo@stanford.edu>
2014-01-22 00:08:29 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Friendswap::Application.config.secret_key_base = '6af211ec77fd0a34b8816801e823d7d6b496a31ab279352eae6c2ac979545d36962dea4892e08d68693a98c14d5f203a78e5c4cdfe2b86e9ed4aa42aa416e07e'
