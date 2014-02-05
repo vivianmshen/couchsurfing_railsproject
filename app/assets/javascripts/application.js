61b8975f5253dd70418f4c2b47b06a420e41e7ed
Erika Ji <erikaji@stanford.edu>
2014-02-04 20:52:01 -0800

    clean install of flatui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,32 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-
-//= require application
-//= require bootstrap.min
-//= require bootstrap-select
-//= require bootstrap-switch
-//= require bootstrap-typeahead
-//= require bootstrap_application
-//= require flatui-checkbox
-//= require flatui-fileinput
-//= require flatui-radio
-//= require holder
-//= require html5shiv
-//= require icon-font-ie7
-//= require jquery.placeholder
-//= require jquery.stacktable
-//= require jquery.tagsinput
-//= require jquery.ui.touch-punch.min
-//= require jquery-1.8.3.min
-//= require jquery-1.10.2.min
-//= require jquery-2.0.3.min
-//= require jquery-ui-1.10.3.custom.min
-//= require prettify
-//= require respond.min
-
+//= require flat-ui
 //= require_tree .
