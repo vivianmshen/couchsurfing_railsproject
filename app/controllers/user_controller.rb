5767623916c792aac13ae001798c96464c719596
vivianshen <vivianmshen@gmail.com>
2014-02-09 22:11:23 -0800

    added dashboard

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,13 @@
 class UserController < ApplicationController
-	
+	def dashboard
+  		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  	end
+
+  	def inbox
+  		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  	end
+
+  	def listings
+  		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  	end
 end
