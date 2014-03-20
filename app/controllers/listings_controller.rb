d58b383ac1f628c83127137192c8c7e42ad84d59
stephanieguo <stguo@stanford.edu>
2014-03-20 00:12:57 -0700

    fix edit, message, review buttons on listing page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -42,6 +42,7 @@ class ListingsController < ApplicationController
 
   #Show an individual listing
   def listing
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
     @listing = Listing.find(params[:id])
     @address = @listing.address
     @user = @listing.user
