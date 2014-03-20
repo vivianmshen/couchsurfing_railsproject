21bb82c34676a0ce1c586af81cb2da6bfd6827f4
Erika Ji <erikaji@stanford.edu>
2014-03-19 23:05:26 -0700

    adding comments

--- app/assets/javascripts/formvalidation.js
+++ app/assets/javascripts/formvalidation.js
@@ -1,38 +1,26 @@
-function validateReviewForm() {
-	var title = document.forms["reviewform"]["title"].value;
-	if (title == null || title == "") {
-		alert("Review title must be filled out.");
-	  return false;
-  }
-	var rating = document.forms["reviewform"]["rating"].value;
-	if (rating == null || rating == "" || rating == 0) {
-		alert("Rating must given.");
-	  return false;
-  }
-  var description = document.forms["reviewform"]["description"].value;
-	if (description == null || description == "") {
-		alert("Review description must be filled out.");
-	  return false;
-  }
-}
+/////////////////////////////////////////////////
+//
+// formvalidation.js
+//
+// This document contains javascript code to cover
+// basic form validation.
+//
+/////////////////////////////////////////////////
 
+
+
+/////////////////////////////////////////////////
+// REQUeST NEW CITY
+// Form location: views/listings/req.html.erb
+/////////////////////////////////////////////////
 function validateNewCityForm() {
 	alert("We've received your request, and we'll work on it as soon as possible. Thank you!");
 }
 
-function validateProfileForm() {
-	var email = document.forms["listingform"]["email"].value;
-	if (email == null || email == "") {
-		alert("Email must be filled out.");
-	  return false;
-  }
-  var bio = document.forms["listingform"]["bio"].value;
-	if (bio == null || bio == "") {
-		alert("User bio must be filled out.");
-	  return false;
-  }
-}
-
+/////////////////////////////////////////////////
+// CREATE LISTING
+// Form location: views/listings/create.html.erb
+/////////////////////////////////////////////////
 function validateListingForm() {
 	var name = document.forms["listingform"]["name"].value;
 	if (name == null || name == "") {
@@ -61,6 +49,10 @@ function validateListingForm() {
   }
 }
 
+/////////////////////////////////////////////////
+// EDIT LISTING
+// Form location: views/listings/edit.html.erb
+/////////////////////////////////////////////////
 function validateListingFormEdit() {
 	var name = document.forms["listingform"]["name"].value;
 	if (name == null || name == "") {
@@ -80,6 +72,45 @@ function validateListingFormEdit() {
   var dates = document.forms["listingform"]["dates"].value;
 	if (dates == null || dates == "") {
 		alert("At least one date must be specified.");
+	  return false;
+  }
+}
+
+/////////////////////////////////////////////////
+// CREATE REVIEW
+// Form location: views/listings/review.html.erb
+/////////////////////////////////////////////////
+function validateReviewForm() {
+	var title = document.forms["reviewform"]["title"].value;
+	if (title == null || title == "") {
+		alert("Review title must be filled out.");
+	  return false;
+  }
+	var rating = document.forms["reviewform"]["rating"].value;
+	if (rating == null || rating == "" || rating == 0) {
+		alert("Rating must given.");
+	  return false;
+  }
+  var description = document.forms["reviewform"]["description"].value;
+	if (description == null || description == "") {
+		alert("Review description must be filled out.");
+	  return false;
+  }
+}
+
+/////////////////////////////////////////////////
+// EDIT PROFILE
+// Form location: views/user/edit.html.erb
+/////////////////////////////////////////////////
+function validateProfileForm() {
+	var email = document.forms["listingform"]["email"].value;
+	if (email == null || email == "") {
+		alert("Email must be filled out.");
+	  return false;
+  }
+  var bio = document.forms["listingform"]["bio"].value;
+	if (bio == null || bio == "") {
+		alert("User bio must be filled out.");
 	  return false;
   }
 }
